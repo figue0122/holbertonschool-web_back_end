51d5f36ac6ee844edf6ae190155643dc959fa160
figue0122 <you@example.com>
2024-02-02 22:41:52 -0400

    2-3 fix spaces

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,13 @@
-#!/usr/bin/node
-export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      resolve();
+export default function handleResponseFromAPI(promise) {
+  return promise
+  // eslint-disable-next-line
+    .then((result) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+  // eslint-disable-next-line
+    .catch((error) => {
+      console.error('An error occured');
+      return new Error();
     });
-  }
+}
